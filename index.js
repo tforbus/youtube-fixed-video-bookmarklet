40b6d144ba2cc30cce6909692debfcc18fd86f26
tforbus <tristinforbus@gmail.com>
2013-10-11 09:01:46 -0500

    bookmarklet url

---
+++ index.js
@@ -0,0 +1,7 @@
+javascript:(function() {
+	var head = document.getElementsByTagName('head')[0]	
+  	, script = document.createElement('script')
+	script.src = '//raw.github.com/tforbus/youtube-fixed-video-bookmarklet/master/script.js'
+	script.type = 'text/javascript'
+	head.appendChild(script)
+})()
